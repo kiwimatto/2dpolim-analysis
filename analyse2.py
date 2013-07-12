a510e3d3e6a6db5ece1ddd7a140ce8c7290299cf
kiwimatto <kiwimatto@gmail.com>
2013-07-12 09:03:45 +0200

    Parallelism now working across valid spots

--- analyse2.py
+++ analyse2.py
@@ -1,7 +1,10 @@
 import sys
 import numpy as np
 from util2dpolim.movie import Movie
-from util2dpolim.misc import grid_image_section_into_squares_and_define_spots, update_image_files, show_mem
+from util2dpolim.misc import grid_image_section_into_squares_and_define_spots, \
+    save_hdf5, \
+    combine_outputs, \
+    show_mem
 import time as stopwatch
 
 from mpi4py import MPI
@@ -10,21 +13,26 @@ myrank = comm.Get_rank()
 nprocs = comm.Get_size()
 
 show_mem()
-
-starttime = stopwatch.time()
-
-prefix = '/home/kiwimatto/Desktop/Lund/2D/2dpolim-analysis/test_data__cool_new_setup_header/'
-
 tstart = stopwatch.time()
 
-# scan whole image, roughly 10 rows at a time
-Nrowsatatime = 10
-for r in np.arange(0,2,Nrowsatatime):
 
-    m = Movie( prefix, 'moviename' )
-    m.define_background_spot( [0,100,50,300] )
 
-    bounds = [ 0, r, 512, r+Nrowsatatime ]
+prefix = '/home/kiwimatto/Desktop/Lund/Experimental/130708/Yuxis_high_conc_sample/'
+basename = 'test2'
+
+bgbounds   = [0,50,60,400]
+fullbounds = [90,60,340,300]
+resolution = 2
+Nrowsatatime = 10*resolution
+
+
+
+for r in np.arange(fullbounds[1], fullbounds[3], Nrowsatatime):
+
+    m = Movie( prefix, basename )
+    m.define_background_spot( bgbounds )
+
+    bounds = [ fullbounds[0], r, fullbounds[2], r+Nrowsatatime ]
     if myrank==0: print 'current bounds: ',bounds
 
     grid_image_section_into_squares_and_define_spots( m, res=1, bounds=bounds )
@@ -33,39 +41,28 @@ for r in np.arange(0,2,Nrowsatatime):
     m.collect_data()
     m.startstop()
     m.assign_portrait_data()
-    #m.are_spots_valid(SNR=4)
+    m.are_spots_valid(SNR=4)
+    # m.fit_all_portraits_spot_parallel()
+    # m.find_modulation_depths_and_phases()
 
-print 'time taken: %fs' % (stopwatch.time()-starttime)
+    myspots = np.array_split( np.arange(len(m.validspots)), nprocs )
 
-raise SystemExit
+    m.fit_all_portraits_spot_parallel_selective( myspots[myrank] )
+    m.find_modulation_depths_and_phases_selective( myspots[myrank] )
 
-m.chew_a_bit(SNR=0)
-
-
-
-
-
-# for si,s in enumerate(m.validspots):
-#     print "si=%d\tLS=%f\tM_ex=%f\tM_em=%f" % (si, s.LS, s.M_ex, s.M_em)
+    # all processes save their contributions separately
+    save_hdf5( m, prefix, myrank )
 
 print 'p=',myrank,': done. ',(stopwatch.time()-tstart)
 
 comm.barrier()
 
-stopwatch.sleep(myrank)
-update_image_files(m, 'mean_intensity', fileprefix=prefix )
-update_image_files(m, 'SNR', fileprefix=prefix )
-update_image_files(m, 'M_ex', fileprefix=prefix )
-update_image_files(m, 'M_em', fileprefix=prefix )
-update_image_files(m, 'phase_ex', fileprefix=prefix )
-update_image_files(m, 'phase_em', fileprefix=prefix )
-update_image_files(m, 'LS', fileprefix=prefix )
-update_image_files(m, 'r', fileprefix=prefix )
-update_image_files(m, 'ET_ruler', fileprefix=prefix )
+# first process gets to combine them into a single file
+if myrank==0: combine_outputs( m.data_basename, prefix )
+
+# all done
 
 
-
-print "time taken: %fs" % (stopwatch.time()-tstart)
 
 
 
